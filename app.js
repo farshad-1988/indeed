0afe3e5fc6b4f35fe0ce74f05315a2f611ba60e1
farshad <far3farazdel@gmail.com>
2024-07-17 20:31:16 +0330

    second commit

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "https://cdnjs.cloudflare.com/ajax/libs/firebase/10.12.3/firebase-app.js";
-
+import { all } from "axios";
+import { initializeApp } from "firebase/app";
+import { deleteDoc, deleteField, getDoc, getDocs, getFirestore, updateDoc } from "firebase/firestore";
+import { doc, setDoc } from "firebase/firestore"
+import { v4 as uuid } from "uuid";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -16,10 +19,128 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const date = new Date()
+const db = getFirestore(app);
+// console.log(uuid().substring(0, 10))
+// const url = `https://de.indeed.com/jobs?q=${searchField}&l=&from=searchOnDesktopSerp`;
+
+document.getElementById("mainForm").addEventListener("submit", async (e) => {
+    e.preventDefault()
+    document.getElementById("showjob").innerHTML = ""
+    document.getElementById("loading").style.display = "block"
+    const jobTitle = e.target.elements.jobTitle.value
+    const newJobTitle = jobTitle.replaceAll(" ", "%20")
+    try {
+        let url = ""
+        if (document.getElementById("serverSource").textContent === "local") {
+            url = "http://localhost:5000/"
+        } else {
+            url = "/.netlify/functions/test"
+        }
+
+        const res = await axios.post(url, { jobTitle: newJobTitle })
+        let data = res.data
+        if (url === "http://localhost:5000/") {
+            data = JSON.parse(data)
+        }
+        data.jobs.forEach((job) => {
+            const div = document.createElement("div")
+            const titlespan = document.createElement("p")
+            const titlelocation = document.createElement("p")
+            const titlecompany = document.createElement("p")
+            titlespan.textContent = job.title
+            titlelocation.textContent = job.location
+            titlecompany.textContent = job.company
+            div.appendChild(titlespan)
+            div.appendChild(titlelocation)
+            div.appendChild(titlecompany)
+            document.getElementById("showjob").appendChild(div)
+            const add = document.createElement("button")
+            add.textContent = "Add"
+            add.addEventListener("click", async (e) => {
+                const jobId = uuid().substring(0, 10)
+                const userRef = doc(db, "jobs", `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`);
+                try {
+                    await updateDoc(userRef, { [jobId]: job })
+                    add.textContent = "Added"
+                } catch (error) {
+                    console.log(error)
+                }
+            })
+            div.appendChild(add)
+        })
+        document.getElementById("loading").style.display = "none"
+    } catch (error) {
+        console.log(error)
+        document.getElementById("loading").style.display = "none"
+    }
+})
+
+document.getElementById("serverSource").addEventListener("click", (e) => {
+    if (e.target.textContent === "local") {
+        e.target.textContent = "netlify"
+    } else {
+        e.target.textContent = "local"
+    }
+})
 
 
-const myf = async () => {
-    const res = await axios.get("/.netlify/functions/test")
-    console.log(res.data)
-}
-myf()
+
+document.getElementById("allJobs").addEventListener("click", async (e) => {
+    const userRef = doc(db, "jobs", `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`);
+    try {
+        document.getElementById("loading").style.display = "block"
+        document.getElementById("showjob").innerHTML = ""
+        const alljobs = await getDoc(userRef)
+        console.log(Object.entries(alljobs.data()))
+        Object.entries(alljobs.data()).forEach((job) => {
+
+            const div = document.createElement("div")
+            const jobId = document.createElement("p")
+            const titlespan = document.createElement("p")
+            const titlelocation = document.createElement("p")
+            const titlecompany = document.createElement("p")
+            jobId.textContent = "ID:" + job[0]
+            titlespan.textContent = job[1].title
+            titlelocation.textContent = job[1].location
+            titlecompany.textContent = job[1].company
+            div.appendChild(jobId)
+            div.appendChild(titlespan)
+            div.appendChild(titlelocation)
+            div.appendChild(titlecompany)
+            document.getElementById("showjob").appendChild(div)
+            const remove = document.createElement("button")
+            remove.textContent = "Remove"
+            remove.addEventListener("click", async (e) => {
+                const userRef = doc(db, "jobs", `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`);
+                try {
+                    await updateDoc(userRef, { [job[0]]: deleteField() })
+                    remove.textContent = "removed successfully"
+                } catch (error) {
+                    console.log(error)
+                }
+            })
+            div.appendChild(remove)
+        })
+        document.getElementById("loading").style.display = "none"
+
+    }
+    catch (error) {
+        document.getElementById("loading").style.display = "none"
+
+        console.log(error)
+    }
+})
+
+// const myf = async () => {
+//     const res = await axios.post("/.netlify/functions/test", { jobTitle })
+//     const userRef = doc(db, "jobs", `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`);
+//     try {
+//         await setDoc(userRef, {
+//             test: "test"
+//         })
+//     } catch (error) {
+//         console.log(error)
+//     }
+// }
+// myf()
--- netlify/functions/test.js
+++ netlify/functions/test.js
@@ -1,6 +1,45 @@
 
+const axios = require('axios');
+const cheerio = require('cheerio');
 
 
-exports.handler = async () => {
-    return { statusCode: 200, body: JSON.stringify({ paymentIntent: "something" }) }
+
+exports.handler = async (info) => {
+
+    // const searchField = "fullstack%20Developer"
+    const { jobTitle } = JSON.parse(info.body)
+    console.log(jobTitle)
+    const url = `https://de.indeed.com/jobs?q=${jobTitle}&l=&from=searchOnDesktopSerp`;
+
+    const apiKey = "be608c635565a46f7d18b61113ffe3df9f2e6cda"
+    // return { statusCode: 200, body: JSON.stringify({ jobTitle }) }
+    // const myfunc = async () => {
+
+    try {
+        const response = await axios({
+            url: 'https://api.zenrows.com/v1/',
+            method: 'GET',
+            params: {
+                'url': url,
+                'apikey': apiKey,
+                'js_render': 'false',
+                'premium_proxy': 'false',
+            },
+        })
+        const $ = cheerio.load(response.data);
+        const jobs = [];
+        $('li.css-5lfssm').each((index, element) => {
+            const title = $(element).find('[id^="jobTitle"]').text().trim();
+            const company = $(element).find('[data-testid="company-name"]').text().trim();
+            const location = $(element).find('[data-testid="text-location"]').text().trim();
+            jobs.push({ company, title, location });
+        })
+        return { statusCode: 200, body: JSON.stringify({ jobs }) }
+    } catch (error) {
+        console.log("error")
+    }
+
+    // }
+    // myfunc()
+
 }
