8142a6d6817309e178337c268e5bca88c99f7400
farshad <far3farazdel@gmail.com>
2024-07-17 11:17:28 +0330

    first

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { initializeApp } from "https://cdnjs.cloudflare.com/ajax/libs/firebase/10.12.3/firebase-app.js";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
